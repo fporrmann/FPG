6d8e6953abc20258fbb96e600bb190ca8c8f93dd
stellalessandra <a.stella@fz-juelich.de>
2021-05-18 14:27:18 +0200

    modified with correct gin command

--- DataAcquisition/preprocess_data.sh
+++ DataAcquisition/preprocess_data.sh
@@ -1,6 +1,6 @@
 cd multielectrode_grasp/datasets
-gin get-content i140703-001-03.nev
-gin get-content i140703-001.odml
+gin download i140703-001-03.nev
+gin download i140703-001.odml
 cd ../..
 python generate_concatenated_data.py
 python create_fpg_input.py
